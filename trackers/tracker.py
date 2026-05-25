462ffb477de2b6d629c50a9ccd5d31ce4d50b46a
MichaelSam3an <michaelsamaananwar@gmail.com>
2026-05-25 15:33:56 +0300

    Update tracker.py

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -473,7 +473,7 @@ class Tracker:
         candidate_distance /= frame_diag
 
         # Same candidate continuing
-    if candidate_distance < 0.03:
+      if candidate_distance < 0.03:
 
         self.ball_candidate_frames += 1
 
@@ -483,15 +483,15 @@ class Tracker:
             self.ball_candidate_frames = 1
 
         # Confirm switch
-        if self.ball_candidate_frames >= self.ball_switch_confirm_frames:
+     if self.ball_candidate_frames >= self.ball_switch_confirm_frames:
 
-            self.previous_ball_bbox = self.ball_candidate_bbox
-            self.ball_missing_frames = 0
-            self.ball_candidate_bbox = None
-            self.ball_candidate_frames = 0
-            self.ball_search_padding = 180
+        self.previous_ball_bbox = self.ball_candidate_bbox
+        self.ball_missing_frames = 0
+        self.ball_candidate_bbox = None
+        self.ball_candidate_frames = 0
+        self.ball_search_padding = 180
 
-            return self.previous_ball_bbox
+        return self.previous_ball_bbox
 
         # Keep previous until switch confirmed
         return self.previous_ball_bbox
